0a6ec2d85f975fa8b90e73027ede13d76d216d78
xelox <alexg@cornerstoneremovals.co.uk>
2022-01-28 19:38:36 +0000

    improved visuals - implemented lose condition (win condition need debugging as is not triggered)

--- public/main.js
+++ public/main.js
@@ -1,22 +1,29 @@
 console.log('hello world');
 
+$(document).bind('contextmenu', function(e)
+{
+    e.preventDefault();
+});
+
 const START_BTN = $('#start_btn');
 
 class Field
 {
     constructor(size)
     {
+        $('#info').css('display', 'none');
         //we define mine field object's properties...
         this.SIZE = size;
         this.rows = []; //this are the rows in the field
         this.field = $('#field');
         $(this.field).html(''); // reset the field
         this.field_virtual = []; //the array containing actual information about the state of boxes in the field;
+        this.lost = false;
 
         for(let i = 0; i < this.SIZE * this.SIZE; i++) //this loop generates all the states for the virual field
         {
             const hasBomb = Math.random() < 0.15;
-            this.field_virtual.push({hasBomb: hasBomb, flipped: false, neighboring: 0}); //here we insert the stat in the virtual field
+            this.field_virtual.push({hasBomb: hasBomb, flipped: false, neighboring: 0, marked: false}); //here we insert the stat in the virtual field
         }
        
         for(let i = 0; i < this.SIZE; i++) //this forloop generates rows
@@ -44,7 +51,8 @@ class Field
                 $(row).append(box);
 
 
-                $(box).click(() => this.solveBoxClick(box));
+                $(box).click(() => this.solveBoxClick(box, true));
+                $(box).bind('contextmenu', () => this.markBox(box));
 
                 $(row).append(box);
             }
@@ -76,12 +84,23 @@ class Field
         return count;
     }
 
-    solveBoxClick(box)
+    solveBoxClick(box, manual)
     {
-        $($(box).find('.box_cover')).css('display', 'none');
+        
         const x = Number($(box).attr('x'));
         const y = Number($(box).attr('y'));
 
+        if(manual )
+        {
+            if(this.field_virtual[y * this.SIZE + x].marked) return;
+            if(this.field_virtual[y * this.SIZE + x].hasBomb) 
+            {  
+                 this.loseGame();
+                 return;
+            }
+        } 
+
+        $($(box).find('.box_cover')).css('display', 'none');
         if(
              this.field_virtual[y * this.SIZE + x].neighboring == 0 && 
             !this.field_virtual[y * this.SIZE + x].flipped
@@ -103,12 +122,47 @@ class Field
                     {
                         const row = this.rows[i];
                         const boxy = $(row).children()[j];
-                        this.solveBoxClick(boxy);
+                        this.solveBoxClick(boxy, false);
                     }
                 }
             }
         }
 
+    }
+
+    markBox(box)
+    {
+        const x = Number($(box).attr('x'));
+        const y = Number($(box).attr('y'));
+        this.field_virtual[y * this.SIZE + x].marked = !this.field_virtual[y * this.SIZE + x].marked;
+        if(this.field_virtual[y * this.SIZE + x].marked)
+            $($(box).find('.box_cover')).html('<span>&#x2753;</span>');
+        else
+            $($(box).find('.box_cover')).html('');
+    }
+
+    loseGame()
+    {
+        $('#info').html('<span>YOU LOST!</span>');
+        $('#info').css('display', 'block');
+        this.lost = true;
+        $($('.box').find('.box_cover')).css('display', 'none');
+    }
+
+    windGame()
+    {
+        flipped_total = 0;
+        hasBomb_total = 0;
+        for (const box of this.field_virtual) {
+            if(box.hasBomb) hasBomb_total++;
+            if(box.flipped) flipped_total++;
+        }
+        if(this.SIZE * this.SIZE - flipped_total - hasBomb_total == 0)
+        {
+            $('#info').html('<span>YOU WON!</span>');
+            $('#info').css('display', 'block');
+            this.won = true;
+        }
     }
 }
 
@@ -118,3 +172,8 @@ $('#size_slider').on('input', function()
     const size = $(this).val();
     field = new Field(size);
 })
+$('#start_btn').click(function()
+{
+    const size = $('#size_slider').val();
+    field = new Field(size);
+});
