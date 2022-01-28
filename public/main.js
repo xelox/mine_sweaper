d0411dc566d9636376852140df709f999c02621f
xelox <alexg@cornerstoneremovals.co.uk>
2022-01-28 16:08:43 +0000

    added a class that generates the field html, and a slider that selects the size of it

--- public/main.js
+++ public/main.js
@@ -1,1 +1,39 @@
 console.log('hello world');
+
+const START_BTN = $('#start_btn');
+
+class Field
+{
+    constructor(size)
+    {
+        //we define mine field object's properties...
+        this.SIZE = size;
+        this.rows = []; //this are the rows in the field
+        this.field = $('#field');
+        $(this.field).html(''); // reset the field
+       
+        for(let i = 0; i < this.SIZE; i++)
+        {
+            const row = document.createElement('div');
+            $(row).addClass('row');
+            $(this.field).append(row);
+            this.rows.push(row);
+
+            for(let j = 0; j < this.SIZE; j++)
+            {
+                const box = document.createElement('div');
+                $(box).addClass('box');
+                $(row).append(box);
+                this.rows.push(box);
+            }
+        }
+
+    }
+}
+
+let field = new Field(10);
+$('#size_slider').on('input', function()
+{
+    const size = $(this).val();
+    field = new Field(size);
+})
