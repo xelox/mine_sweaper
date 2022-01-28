4af3e8e368541d18deee80123426e9e22ff199b6
xelox <alexg@cornerstoneremovals.co.uk>
2022-01-28 15:25:30 +0000

    Init

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express")
+const app = express();
+const port = 3000;
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => 
+{
+    res.render('main');
+})
+
+app.get('/public/:file', (req, res)=>
+{
+    res.sendFile(path.join(__dirname, 'public', `${req.params.file}`));
+});
+
+app.get('/favicon.ico', (req, res) =>
+{
+    res.sendFile(path.join(__dirname, 'public', `favicon.ico`));
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
